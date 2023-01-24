0df61bff833247b0f692050be8f4b624724e114e
Marco Karpf <marco.karpf@swisscom.com>
2023-01-24 23:02:08 +0100

    initial setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UsersListComponent} from "./components/users-list/users-list.component";
+import {UserDetailsComponent} from "./components/user-details/user-details.component";
+import {AddUserComponent} from "./components/add-user/add-user.component";
+import {BookingDetailsComponent} from "./components/booking-details/booking-details.component";
+import {BookingListComponent} from "./components/booking-list/booking-list.component";
+import {LoginComponent} from "./components/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'users', component: UsersListComponent },
+  { path: 'users/:id', component: UserDetailsComponent },
+  { path: 'add-user', component: AddUserComponent },
+  { path: "bookings", component: BookingListComponent },
+  { path: "bookings/:id", component: BookingDetailsComponent },
+  { path: "bookings/user/:username", component: BookingListComponent },
+  {path: "login", component: LoginComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { UsersListComponent } from './components/users-list/users-list.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {BookingDetailsComponent} from "./components/booking-details/booking-details.component";
+import {BookingListComponent} from "./components/booking-list/booking-list.component";
+import {AddBookingComponent} from "./components/add-booking/add-booking.component";
+import {LoginComponent} from "./components/login/login.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent,
+    UserDetailsComponent,
+    UsersListComponent,
+    BookingListComponent,
+    AddBookingComponent,
+    BookingDetailsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
